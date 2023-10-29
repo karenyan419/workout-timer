c595bc39d580f9a9b9d284e6f1fd7e0ea5aecbd3
Karen Yan <karen.yan419@gmail.com>
2023-10-29 22:59:45 +0000

    convert seconds to hr/min/s format

--- src/TimeDisplay.tsx
+++ src/TimeDisplay.tsx
@@ -1,9 +1,26 @@
 import { useEffect, useState } from "react"
 
 const TimeDisplay = (props: {time: number})  => {
+
+
+    const processNumbers = (input: number) => {
+        const hours: number = Math.floor(input/3600)
+        const minutes: number = Math.floor((input-hours*3600)/60)
+        const seconds: number = input-hours*3600-minutes*60
+        const addZeroForTwoDigits = (input: number) => {
+            if (input.toString().length< 2) {
+                return "0" + input
+            }
+        }
+        return `${hours}:${addZeroForTwoDigits(minutes)}:${addZeroForTwoDigits(seconds)}`
+
+    }
+
+    
+
     
     return <div>
-        {props.time}
+        {processNumbers(props.time)}
     </div>
 };
 
