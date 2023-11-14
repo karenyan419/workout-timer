aa41f5c94dc2e1cfee121281efe97d948fe74668
Karen Yan <karen.yan419@gmail.com>
2023-11-14 20:56:16 +0000

    fixed processNumbers function

--- src/TimeDisplay.tsx
+++ src/TimeDisplay.tsx
@@ -8,9 +8,10 @@ const TimeDisplay = (props: {time: number})  => {
         const minutes: number = Math.floor((input-hours*3600)/60)
         const seconds: number = input-hours*3600-minutes*60
         const addZeroForTwoDigits = (input: number) => {
-            if (input.toString().length< 2) {
+            if (input.toString().length < 2) {
                 return "0" + input
             }
+            return input
         }
         return `${hours}:${addZeroForTwoDigits(minutes)}:${addZeroForTwoDigits(seconds)}`
 
