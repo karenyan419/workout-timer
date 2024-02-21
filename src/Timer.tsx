f10a9efdbe4f41bb998b06a81230f32b7420a5d3
Karen Yan <karen.yan419@gmail.com>
2024-02-21 18:52:55 +0000

    remaining timesUp changes

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react"
 import TimeDisplay from "./TimeDisplay"
 import StartStop from "./StartStop"
 import Reset from "./Reset";
+import TimesUp from "./TimesUp";
 
 const Timer = ()  => {
 
-    const [timerDuration, setTimerDuration] = useState<number>(10);
+    const [timerDuration, setTimerDuration] = useState<number>(5);
     const [time, setTime] = useState<number>(timerDuration);
     const [timerActive, setTimerActive] = useState<boolean>(false);
+    const [timesUp, setTimesUp] = useState<boolean>(false);
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -18,20 +20,21 @@ const Timer = ()  => {
         return () => clearInterval(interval);
       }, [timerActive]);
 
-    const clickHandler = () => {
-        setTime(0);
-    }
-    console.log(time)
+
     if (timerActive == true && time < 0) {
       setTime(0);
       setTimerActive(false);
+      setTimesUp(true);
     }
     
     return <>
         <TimeDisplay time={time}/>
         <div>
           <StartStop timerActive={timerActive} setTimerActive={setTimerActive}/>
-          <Reset setTime={setTime} timerDuration={timerDuration} setTimerActive={setTimerActive}/>
+          <Reset setTime={setTime} timerDuration={timerDuration} setTimerActive={setTimerActive} setTimesUp={setTimesUp}/>
+        </div>
+        <div>
+        <TimesUp status={timesUp}/>
         </div>
     </>
 }
